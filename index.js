7a9ceb1f824ef87d3173605af28b68ed74990ce9
Alvin Mahendra <yusufalvin0799@gmail.com>
2023-05-12 12:37:56 +0700

    changed port

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(8001, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 8001');
 });
